93e288ba63dc16cb6a8a00db00ce619931e0b1a2
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-06-03 18:59:58 -0300

    Mudanças visuais

--- src/layout/header.js
+++ src/layout/header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 import { Nav, NavItem, NavLink } from 'reactstrap'
 import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react'
 
-import logo from '../assets/img/brand/logo.png'
-import logoShort from '../assets/img/brand/logoShort.svg'
+import logo from '../assets/img/brand/logo_fifa.svg'
+import logoShort from '../assets/img/brand/logo_fifa_image.svg'
 
 class Header extends Component {
 	render() {
@@ -12,8 +12,8 @@ class Header extends Component {
 			<React.Fragment>
 				<AppSidebarToggler className='d-lg-none' display='md' mobile />
 				<AppNavbarBrand
-					full={{ src: logo, width: 145, height: 20, alt: 'CoreUI Logo' }}
-					minimized={{ src: logoShort, width: 40, height: 15, alt: 'CoreUI Logo' }}
+					full={{ src: logo, width: 145, height: 45, alt: 'CoreUI Logo' }}
+					minimized={{ src: logoShort, width: 40, height: 45, alt: 'CoreUI Logo' }}
 				/>
 				<AppSidebarToggler className='d-md-down-none' display='lg' />
 				<Nav className='d-md-down-none' navbar>

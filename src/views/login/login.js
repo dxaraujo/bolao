0810535513b1eae0c85062f61a11da3777326512
daniel.xavier@dataprev.gov.br <daniel.xavier@dataprev.gov.br>
2018-06-07 15:07:17 -0300

    Correção de font

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login'
 
-import { Card, CardBody, CardGroup, Col, Container, Row } from 'reactstrap';
+import { Card, CardBody, Col, Container, Row } from 'reactstrap';
 
 import withAuth from '../../components/withAuth'
-import logo from '../../assets/img/brand/logo.png'
 
 class Login extends Component {
 
@@ -30,37 +29,31 @@ class Login extends Component {
 	render() {
 		return (
 			<div className="app flex-row align-items-center">
-				<Container>
+				<Container fluid>
 					<Row className="justify-content-center">
-						<Col md="8">
-							<CardGroup>
-								<Card className="p-4">
-									<CardBody>
-										<Row style={{ marginBottom: '5px', textAlign: 'center' }}>
-											<Col xs="12">
-												<img style={{ objectFit: 'contain' }} src={logo} alt="Logo" width="100%" height="auto" />
-												<FacebookLogin
-													appId="393226907829156"
-													fields="id, name,email,picture"
-													autoLoad={false}
-													disableMobileRedirect={true}
-													textButton={' Entrar com Facebook'}
-													callback={this.loginWithFacebook}
-													cssClass="btn btn-primary btn-lg mt-3"
-													icon="fab fa-facebook"
-												/>
-												<p className="text-muted mt-3">Realize o login com a sua conta do Facebook</p>
-											</Col>
-										</Row>
-									</CardBody>
-								</Card>
-								<Card className="text-white bg-primary d-md-down-none">
-									<CardBody className="text-center align-middle" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-										<h2>Entrar com Facebook Disponível</h2>
-										<p>Agora você pode fazer o login usando Facebook</p>
-									</CardBody>
-								</Card>
-							</CardGroup>
+						<Col md="4">
+							<Card style={{ marginBottom: '0px' }}>
+								<CardBody className="russia2018">
+								</CardBody>
+							</Card>
+							<Card>
+								<CardBody className="d-flex flex-column align-items-center justify-itens-center p-0">
+									<h1 style={{ fontFamily: 'proximanova-bold' }}>Bolão Copa do Mundo</h1>
+									<div className="align-content-center">
+										<FacebookLogin
+											appId="393226907829156"
+											fields="id, name,email,picture"
+											autoLoad={false}
+											disableMobileRedirect={true}
+											textButton={' Entrar com Facebook'}
+											callback={this.loginWithFacebook}
+											cssClass="btn btn-primary btn-lg mt-3"
+											icon="fab fa-facebook"
+										/>
+									</div>
+									<p className="text-muted mt-3">Realize o login com a sua conta do Facebook</p>
+								</CardBody>
+							</Card>
 						</Col>
 					</Row>
 				</Container>

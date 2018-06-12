f04e6a4f43aba49071fc532bdd6312a6ced2d486
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-06-12 18:56:49 -0300

    Test Login on mobile

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -44,6 +44,7 @@ class Login extends Component {
 											appId="393226907829156"
 											fields="id,name,email,picture"
 											autoLoad={false}
+											disableMobileRedirect={true}
 											textButton={' Entrar com Facebook'}
 											callback={this.loginWithFacebook}
 											cssClass="btn btn-primary btn-lg mt-3"

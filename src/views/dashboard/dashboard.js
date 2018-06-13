ca8812d30f1e54385198a35b5f9c0eadf1ad2666
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-06-12 23:08:59 -0300

    Correção da seleção de times

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -54,8 +54,8 @@ const chartPieData = {
 			backgroundColor: [
 				'rgb(75, 192, 192)',
 				'rgb(54, 162, 235)',
-				'rgb(255, 159, 64)',
 				'rgb(255, 205, 86)',
+				'rgb(255, 159, 64)',
 				'rgb(255, 99, 132)',
 			],
 			data: [],

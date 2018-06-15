795dcf8beeae7b73f6bd4871eebaeeb4e8d53e92
daniel.xavier@dataprev.gov.br <daniel.xavier@dataprev.gov.br>
2018-06-15 09:25:14 -0300

    Desabilitando service workes

--- src/index.js
+++ src/index.js
@@ -27,5 +27,5 @@ ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>
-, document.getElementById('root'))
-registerServiceWorker();
+	, document.getElementById('root'))
+//registerServiceWorker();

f05490c89f39e13d07740e298bb27690b9cd2c3a
Daniel Xavier Araujo - DATAPREVCE <daniel.xavier@dataprev.gov.br>
2022-11-13 22:36:37 -0300

    Ajuste do yarn.lock

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 const backendURI = process.env.REACT_APP_BACKEND_URI || 'http://127.0.0.1:3001'
-const rootUser = 'danielxaraujo@gmail.com12'
+const rootUser = process.env.REACT_APP_ROOT_USER || 'danielxaraujo@gmail.com'
 export { backendURI, rootUser }
